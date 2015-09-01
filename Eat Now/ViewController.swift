456ccec97fbc3af7cb3efca20c1e20506c1dd9cb
shuoshan <shanshuo0918@gmail.com>
2015-08-31 23:34:47 -0700

    first commit

---
+++ Eat Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eat Now
+//
+//  Created by SHUO SHAN on 8/25/15.
+//  Copyright (c) 2015 Shuo Shan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
